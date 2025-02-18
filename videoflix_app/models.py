9f0d22648f29588461fbcde7a818b34f6ad73cb3
Marius Kasparek <marius.kasparek@gmail.com>
2025-02-18 14:20:56 +0100

    added video model

--- videoflix_app/models.py
+++ videoflix_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+class Video(models.Model):
+    created_at = models.DateField(default=date.today)
+    title = models.CharField(max_length=80)
+    description = models.CharField(max_length=500)
+    video_file = models.FileField(upload_to='videos', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
