b1b3c80af2e33c75956ccb54f5a9762747d7d9c4
Marius Kasparek <marius.kasparek@gmail.com>
2025-02-19 13:55:04 +0100

    added LoginView and url

--- user_auth_app/api/urls.py
+++ user_auth_app/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RegistrationView
+from .views import RegistrationView, LoginView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration'),
+    path('login/', LoginView.as_view(), name='login'),
     
 ]
--- user_auth_app/api/views.py
+++ user_auth_app/api/views.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from rest_framework.authtoken.views import ObtainAuthToken
 
 
+
 class RegistrationView(APIView):
     permission_classes = [AllowAny]
 
@@ -23,5 +24,25 @@ class RegistrationView(APIView):
             }
         else:
             data=serializer.errors
+        
+        return Response(data)
+
+
+class LoginView(ObtainAuthToken):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        data = {}
+
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token, created = Token.objects.get_or_create(user=user)
+            data = {
+                'token': token.key,
+                'email': user.email,
+            }
+        else:
+            data = serializer.errors
         
         return Response(data)
