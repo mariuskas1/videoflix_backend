314a041732ff1a5d74c43d91542084c8589eecfd
Marius Kasparek <marius.kasparek@gmail.com>
2025-02-19 10:17:06 +0100

    modified admin.py to include ImportExport functionality

--- videoflix_app/admin.py
+++ videoflix_app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from .models import Video
 
-# Register your models here.
+
+
+class VideoResource(resources.ModelResource):
+    class Meta:
+        model = Video
+
+
+class VideoAdmin(ImportExportModelAdmin):
+    resource_class = VideoResource
+
+admin.site.register(Video, VideoAdmin)
