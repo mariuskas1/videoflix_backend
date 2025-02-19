7c7f2e8b74337e27ad74f7e21fdeb8cc35ab5ea8
Marius Kasparek <marius.kasparek@gmail.com>
2025-02-19 09:34:26 +0100

    used task queue for video converting

--- videoflix_app/signals.py
+++ videoflix_app/signals.py
@@ -3,15 +3,16 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
 import os
 from videoflix_app.tasks import convert_720p
+import django_rq
 
 
 @receiver(post_save, sender=Video)
 def video_post_save(sender, instance, created, **kwargs):
-    print('Ok Video')
-
+    
     if created:
-        print('Video created')
-        convert_720p(instance.video_file.path)
+        queue = django_rq.get_queue('default', autocommit=True)
+        queue.enqueue(convert_720p, instance.video_file.path)
+        
 
 
 @receiver(post_delete, sender=Video)
