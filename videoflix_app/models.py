731c71c26236b362a9af770f5e3044257d79b0a2
Marius Kasparek <marius.kasparek@gmail.com>
2025-02-19 14:11:33 +0100

    expanded Video Model

--- videoflix_app/models.py
+++ videoflix_app/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from datetime import date
 
+
+GENRES = [
+    ('documentary', 'Documentary'),
+    ('drama', 'Drama'),
+    ('romance', 'Romance'),
+]
+
 class Video(models.Model):
     created_at = models.DateField(default=date.today)
     title = models.CharField(max_length=80)
+    genre = models.CharField(max_length=80, choices=GENRES, default='documentary')
     description = models.CharField(max_length=500)
     video_file = models.FileField(upload_to='videos', blank=True, null=True)
+    thumbnail = models.FileField(upload_to='thumbnails', null=True, blank=True)
 
     def __str__(self):
         return self.title
