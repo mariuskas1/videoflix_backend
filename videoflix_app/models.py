94f64b7a72f46cbb006cc522c5766f3570af7bc9
Marius Kasparek <marius.kasparek@gmail.com>
2025-02-24 09:50:57 +0100

    improved video model

--- videoflix_app/models.py
+++ videoflix_app/models.py
@@ -12,7 +12,7 @@ class Video(models.Model):
     created_at = models.DateField(default=date.today)
     title = models.CharField(max_length=80)
     genre = models.CharField(max_length=80, choices=GENRES, default='documentary')
-    description = models.CharField(max_length=500)
+    description = models.CharField(max_length=500, blank=True, null=True)
     video_file = models.FileField(upload_to='videos', blank=True, null=True)
     thumbnail = models.FileField(upload_to='thumbnails', null=True, blank=True)
     new = models.BooleanField(default=False)
