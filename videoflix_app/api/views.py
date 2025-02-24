e1f9282c2670811a30a9ee8a28fbf88514449ba9
Marius Kasparek <marius.kasparek@gmail.com>
2025-02-24 11:14:42 +0100

    changed permissions for testing

--- videoflix_app/api/views.py
+++ videoflix_app/api/views.py
@@ -1,14 +1,14 @@
 from rest_framework import viewsets
 from videoflix_app.models import Video
 from .serializers import VideoSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
 
 class VideoViewset(viewsets.ModelViewSet):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
     def get_serializer_context(self):
         return {'request': self.request}
