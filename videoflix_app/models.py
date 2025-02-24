3933641d3fd038e0624a821ce11d4892318d138d
Marius Kasparek <marius.kasparek@gmail.com>
2025-02-24 13:44:33 +0100

    added custom save method to video model for resizing thumbnail images

--- videoflix_app/models.py
+++ videoflix_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from datetime import date
+from PIL import Image
+import os
 
 
 GENRES = [
@@ -16,6 +18,16 @@ class Video(models.Model):
     video_file = models.FileField(upload_to='videos', blank=True, null=True)
     thumbnail = models.FileField(upload_to='thumbnails', null=True, blank=True)
     new = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.thumbnail:
+            img_path = self.thumbnail.path
+            img = Image.open(img_path)
+
+            img.thumbnail((213, 120))
+            img.save(img_path, quality=90)
 
     def __str__(self):
         return self.title
