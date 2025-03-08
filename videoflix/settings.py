1634265621b5d510d8f8f6d5d55873410ec6ae72
Marius Kasparek <marius.kasparek@gmail.com>
2025-03-08 12:12:23 +0100

    added postgresql database

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -134,11 +134,14 @@ WSGI_APPLICATION = 'videoflix.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'videoflix',  
+        'USER': 'marius',      
+        'PASSWORD': 'Passwort', 
+        'HOST': 'localhost',   
+        'PORT': '5432',        
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
