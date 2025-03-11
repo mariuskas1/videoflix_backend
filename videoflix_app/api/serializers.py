4ac33caf7c4390deafe4da17b7772873ffc5e0cc
Marius Kasparek <marius.kasparek@gmail.com>
2025-03-11 10:33:35 +0100

    expanded video model for new resolutions

--- videoflix_app/api/serializers.py
+++ videoflix_app/api/serializers.py
@@ -7,13 +7,13 @@ class VideoSerializer(serializers.ModelSerializer):
         model = Video
         fields = '__all__'
 
-    def to_representation(self, instance):
-        """Customize serialized output to conditionally include 'video_file'."""
-        representation = super().to_representation(instance)
+    # def to_representation(self, instance):
+    #     """Customize serialized output to conditionally include 'video_file'."""
+    #     representation = super().to_representation(instance)
 
-        request = self.context.get('request')
-        if request and request.parser_context:
-            if request.parser_context['view'].action == 'list':
-                representation.pop('video_file', None)
+    #     request = self.context.get('request')
+    #     if request and request.parser_context:
+    #         if request.parser_context['view'].action == 'list':
+    #             representation.pop('video_file', None)
 
-        return representation
+    #     return representation
