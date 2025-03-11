1e8f7b3106918ae03fc37a50f9ce88164de75e3b
Marius Kasparek <marius.kasparek@gmail.com>
2025-03-11 16:59:25 +0100

    updated tasks.py and video serializer

--- videoflix_app/api/serializers.py
+++ videoflix_app/api/serializers.py
@@ -1,19 +1,40 @@
 from rest_framework import serializers
+from django.conf import settings
 from videoflix_app.models import Video
+import os
 
 
 class VideoSerializer(serializers.ModelSerializer):
+    video_file_120p = serializers.SerializerMethodField()
+    video_file_360p = serializers.SerializerMethodField()
+    video_file_720p = serializers.SerializerMethodField()
+    video_file_1080p = serializers.SerializerMethodField()
+
     class Meta:
         model = Video
         fields = '__all__'
 
-    # def to_representation(self, instance):
-    #     """Customize serialized output to conditionally include 'video_file'."""
-    #     representation = super().to_representation(instance)
+    def get_video_file_120p(self, obj):
+        return self.get_video_url(obj.video_file, 120)
 
-    #     request = self.context.get('request')
-    #     if request and request.parser_context:
-    #         if request.parser_context['view'].action == 'list':
-    #             representation.pop('video_file', None)
+    def get_video_file_360p(self, obj):
+        return self.get_video_url(obj.video_file, 360)
 
-    #     return representation
+    def get_video_file_720p(self, obj):
+        return self.get_video_url(obj.video_file, 720)
+
+    def get_video_file_1080p(self, obj):
+        return self.get_video_url(obj.video_file, 1080)
+
+    def get_video_url(self, video_path, resolution):
+        if not video_path:
+            return None
+        
+        base_name = os.path.basename(video_path.url)  
+        converted_path = f"media/videos/{base_name.replace('.mp4', f'_{resolution}p.mp4')}"
+        
+        
+        if os.path.exists(os.path.join(settings.MEDIA_ROOT, f"videos/{converted_path}")):
+            return f"{settings.MEDIA_URL}{converted_path}"
+        
+        return None
